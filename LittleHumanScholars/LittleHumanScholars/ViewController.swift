faed1294e901dc992e4f80d51bc75b378681604d
tatsean <tatsean1977@gmail.com>
2015-11-18 05:05:07 +0800

    Screens Design

---
+++ LittleHumanScholars/LittleHumanScholars/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LittleHumanScholars
+//
+//  Created by Tat Sean Pang on 16/11/2015.
+//  Copyright © 2015 Tat Sean Pang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var classTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
